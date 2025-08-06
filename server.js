8273d407ace124ba6afef10397c80bdba7f25e09
solafaissa12 <solafaissa1@gmail.com>
2025-08-06 15:16:19 +0300

    change headers

--- server.js
+++ server.js
@@ -16,10 +16,11 @@ connectDB();
 app.use(logger);
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"),
+  res.header("Access-Control-Allow-Origin", "https://frontsection.netlify.app/"),
     res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
+//"http://localhost:3000"
 
 app.use(cors(corsOptions));
 //app.use(cors());
